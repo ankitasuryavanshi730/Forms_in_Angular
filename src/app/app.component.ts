1b25d41dd86901baa35c089d55e275fd52e08b10
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-26 21:31:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Forms';
+
+ country = [
+
+  {id:1, name:"India"},
+
+  {id:2, name:"America"},
+
+  {id:3, name:"Japan"},
+
+  {id:4, name:"london"}
+ ]
+submit(data:any){
+  if(data.username == "ankita" && data.password == 12345){
+    alert("welcome to the Angular Form")
+  }
+  else{
+    alert("your name and password is invalid")
+  }
+
+  console.log(data);
+
+
+}
+
+  submitin(data:any){
+    if(data.password == "ankita"){
+      alert("you are correct")
+    }
+    else{
+      alert("your password in incorrect")
+    }
+
+    console.log(data);
+
+  }
+
+
+  allsubmit(data:any){
+
+    console.log(data);
+
+
+  }
+
+  sub(data:any){
+   if(data.username == "Ankita Suryavanshi" && data.password == 987654321){
+    alert("welcome to the forms")
+   }
+   else{
+    alert("invalid information")
+   }
+  }
+
 }
